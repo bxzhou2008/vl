8bf77acc1f5a4d1ac0e9606096fd111ff0577649
bunsen <bunsen.job@gmail.com>
2022-05-26 19:23:25 +0800

    update

---
+++ src/pages/login.vue
@@ -0,0 +1,15 @@
+<script setup>
+const mobile = ref('')
+const passwd = ref('')
+</script>
+
+<template>
+  <div class="page" w="100%" h="100%" bg-green flex="~ gap-2" justify-center items-center>
+    <div class="box" w-100 h-100 b-1 bg-red flex justify-center items-center>
+      <div flex flex-row>
+        <i i-mdi-vuejs w-40 h-40 />
+        <input id="" type="text" name="">
+      </div>
+    </div>
+  </div>
+</template>
